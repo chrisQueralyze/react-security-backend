13a02e52b355cfc688f689d7baa428eff03d37b9
Watson, Christopher <chris@queralyze.com>
2018-07-15 15:58:58 -0400

    change creds

--- secrets/db_configuration.js
+++ secrets/db_configuration.js
@@ -1,10 +1,10 @@
 let user, host, database, password;
 
 if (process.env.MODE === 'production') {
-  user = 'dbwbapmsdoowtw';
-  host = 'ec2-23-23-247-222.compute-1.amazonaws.com';
+  user = 'gmjndvsdglgvmb';
+  host = 'ec2-54-227-240-7.compute-1.amazonaws.com';
   database = 'd6k1oq1ia248qd';
-  // password = '9ee8f394c7fcaea7b9b803d15f3fab969abccd75356e4691e9159d447132ca02';
+  password = '4f6b808e42e2b3b2fa616b6d5659e603b0cf5f3ce27ef0c067eed9653cd6f257';
   password = process.env.DB_PASSWORD;
 } else {
   user = 'node_user';
