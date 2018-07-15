e0d34b5a36fd48d4cc4aa69782d121875f2b9f4f
Watson, Christopher <chris@queralyze.com>
2018-07-15 16:38:09 -0400

    init commit 2

--- secrets/db_configuration.js
+++ secrets/db_configuration.js
@@ -1,10 +1,10 @@
 let user, host, database, password;
 
 if (process.env.MODE === 'production') {
-  user = 'gmjndvsdglgvmb';
-  host = 'ec2-54-227-240-7.compute-1.amazonaws.com';
-  database = 'd6k1oq1ia248qd';
-  password = '4f6b808e42e2b3b2fa616b6d5659e603b0cf5f3ce27ef0c067eed9653cd6f257';
+  user = 'gphqoqpoyitcmo';
+  host = 'ec2-54-83-33-213.compute-1.amazonaws.com';
+  database = 'ddd8jp2cfmddtc';
+  password = 'fd230e3a1cadfccb1cf6263b1d8bf959bdec7bdafa47404af46acdae65c42ae0';
   password = process.env.DB_PASSWORD;
 } else {
   user = 'node_user';
